ded7e3649e8f44470feab847fa4dc615a27c5c85
Greg Lubin <greg@greglubin.com>
2014-11-21 17:58:47 -0600

    Updated counter and basic functioning with Zeev

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
+var pastWords = [];
 var potentials = {};
 var mySockets = {};
 var votes = {};
+var counter = 10;
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
@@ -14,8 +15,9 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   // console.log(socket);
   mySockets[socket.id] = socket;
+  console.log(new Date().getTime());
 
-  socket.emit('initialize', votes);
+  socket.emit('initialize', votes, pastWords);
   
   // socket.on('get chat history', function(msg) {
   //  // io.emit('initialize', pastWords);  
@@ -54,6 +56,71 @@ io.on('connection', function(socket){
     });
 });
 
+function updateWord(){
+    var empty = true;
+    for (var key in votes){
+      if (votes.hasOwnProperty(key)){
+         empty = false;
+         break;
+      }
+    }
+    if (empty){
+        return;
+    }
+
+    var voteToAdd = 0;
+    var wordToAdd = "";
+    for (var word in votes){
+      if (votes[word] > voteToAdd){
+        voteToAdd = votes[word];
+        wordToAdd = word;
+      }
+    }
+
+    io.emit('server update', wordToAdd);
+    pastWords.push(wordToAdd);
+    votes = {};
+    potentials = {};
+
+}
+
+
+  function updateTime(){
+
+    counter --;
+    io.emit('update time', counter);
+    if (counter === 0){
+      updateWord();
+      counter = 10;
+    }
+
+  }
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
+  setInterval(updateTime,1000);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
